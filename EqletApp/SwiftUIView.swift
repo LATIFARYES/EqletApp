24cb37741504470582ba7be6fec843bb86dda55d
itsEngRaghad <132268219+itsEngRaghad@users.noreply.github.com>
2023-10-09 15:52:20 +0300

    my updates

---
+++ EqletApp/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  EqletApp
+//
+//  Created by Raghad on 09/10/2023.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
