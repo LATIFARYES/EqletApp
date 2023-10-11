9e3d1cdc542fb1d9fe9fb7ac1aeae991ed4459cf
Razan Alabdulkarim <razankr21@gmail.com>
2023-10-11 17:10:15 +0300

    add infroPage class

---
+++ EqletApp/introPage.swift
@@ -0,0 +1,18 @@
+//
+//  introPage.swift
+//  EqletApp
+//
+//  Created by Razan Alabdulkarim on 26/03/1445 AH.
+//
+
+import SwiftUI
+
+struct introPage: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+#Preview {
+    introPage()
+}
