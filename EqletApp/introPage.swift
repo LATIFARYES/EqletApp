83549f3a33da446d573390e8753de4421495ec53
itsEngRaghad <132268219+itsEngRaghad@users.noreply.github.com>
2023-10-11 17:14:03 +0300

    color changing
    ..

--- EqletApp/introPage.swift
+++ EqletApp/introPage.swift
@@ -10,9 +10,27 @@ import SwiftUI
 struct introPage: View {
     var body: some View {
         Text("Hello")
+            .foregroundColor(Color(hex: "#6A5C46"))
     }
 }
 
 #Preview {
     introPage()
+}
+
+extension Color {
+    init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
+        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = Double(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue)
+    }
 }
