607500e0d3b5e8032aa95f0f738c93c1e3bce5fd
Razan Alabdulkarim <razankr21@gmail.com>
2023-10-12 15:58:15 +0300

    fix padding logo

--- EqletApp/introPage.swift
+++ EqletApp/introPage.swift
@@ -7,44 +7,28 @@
 
 import SwiftUI
 
-struct introPage: View {
+struct IntroPage: View {
     var body: some View {
-        //        Text("Hello")
-        //            .foregroundColor(Color(hex: "#6A5C46"))
-        
         ZStack{
             Image("introBG")
                 .resizable()
                 .scaledToFill()
                 .edgesIgnoringSafeArea(.all)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .background(Color.clear)
             
             Image("logo")
                 .resizable()
                 .scaledToFit()
                 .frame(width: 250)
-            
+                .padding(.leading, 15)
         }
     }
-    
 }
 
-#Preview {
-    introPage()
-}
-
-extension Color {
-    init(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
-        var rgb: UInt64 = 0
-
-        Scanner(string: hexSanitized).scanHexInt64(&rgb)
-
-        let red = Double((rgb & 0xFF0000) >> 16) / 255.0
-        let green = Double((rgb & 0x00FF00) >> 8) / 255.0
-        let blue = Double(rgb & 0x0000FF) / 255.0
-
-        self.init(red: red, green: green, blue: blue)
+struct IntroPage_Previews: PreviewProvider {
+    static var previews: some View {
+        IntroPage()
     }
 }
+
