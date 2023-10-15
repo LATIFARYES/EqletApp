ee065f604788ccbc4d0bedc7bef4dbacdc7b6027
NoufAlmtrek <noufalmtrek@gmail.com>
2023-10-15 17:49:10 +0300

    Revert "add tab bar"
    This reverts commit 6dbde06c93efbfbc9e6964b384d34b5e4d1404c2.

--- EqletApp/ContentView.swift
+++ EqletApp/ContentView.swift
@@ -9,31 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView {
-            
-            
-            
-            Text(" ")
-                
-            
-                .tabItem {
-                    Image( "cameltrans")
-                    Text("Map")
-                }.tag(0)
-           Text(" landmarkes ")
-                .tabItem {
-                    Image( "castel")
-                    Text("Landmarkes")
-                }.tag(1)
-            Text("Hame Tab ")
-                .tabItem {
-                    Image(  "xxx")
-                    Text("My Journyes")
-                    
-                }.tag(2)
-           
-           
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
