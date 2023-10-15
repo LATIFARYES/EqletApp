30f63b8bfb2c7f7fbbe5da84b98254a1993f88e5
WajdAlhadlaq <86900714+WajdAlhadlaq@users.noreply.github.com>
2023-10-15 17:57:20 +0300

    Fix the style of landmarks cardview
    Add Northern region landmarks information

--- EqletApp/Attractions.swift
+++ EqletApp/Attractions.swift
@@ -172,17 +172,19 @@ struct  Landmark : Identifiable{
 }
     var landmarks = [
         
-        Landmark(landmarkTitle: "Madain Saleh", landmarkBrief: "It is located in the north of Madinah Province, in the area of Al-Ula, where many other fascinating archeological sites attest to the thousands of years of human occupation of the area thanks to the underground water available for millennia.", landmarkImage:"MadainSaleh",landmarkLocation: "Al-Ula",landmarkDistance:"1045 KM"),
         
-        Landmark(landmarkTitle: "Madain Saleh", landmarkBrief: "It is located in the north of Madinah Province, in the area of Al-Ula, where many other fascinating archeological sites attest to the thousands of years of human occupation of the area thanks to the underground water available for millennia.", landmarkImage:"MadainSaleh",landmarkLocation: "Al-Ula",landmarkDistance:"1045 KM"),
+        Landmark(landmarkTitle: "Jubbah", landmarkBrief: "Jubbah, located 90 km north of the city of Ha’il is the most famous rock art site in Saudi Arabia. The carvings that can be found on the Jibal Umm Sinman cover a wide range of representations as well as a large period of time with some carving possibly being 10 000 years old. Indeed, human representations are found along with cattle, ibexes, oryxes, deers, gazelles, horses, but also lions and ostriches. Camels are probably the latest to populate both this area and the walls of the surrounding rocks.", landmarkImage:"Jubbah",landmarkLocation: "Hail",landmarkDistance:"732 KM"),
         
-        Landmark(landmarkTitle: "Madain Saleh", landmarkBrief: "It is located in the north of Madinah Province, in the area of Al-Ula, where many other fascinating archeological sites attest to the thousands of years of human occupation of the area thanks to the underground water available for millennia.", landmarkImage:"MadainSaleh",landmarkLocation: "Al-Ula",landmarkDistance:"1045 KM"),
-        
-        
-        Landmark(landmarkTitle: "Madain Saleh", landmarkBrief: "It is located in the north of Madinah Province, in the area of Al-Ula, where many other fascinating archeological sites attest to the thousands of years of human occupation of the area thanks to the underground water available for millennia.", landmarkImage:"MadainSaleh",landmarkLocation: "Al-Ula",landmarkDistance:"1045 KM"),
+        Landmark(landmarkTitle: "Marid Castle", landmarkBrief: "The castle Marid (which means “the rebel” in Arabic) stands on a natural limestone outcrop overlooking the valley where the ancient oasis lies. Its position, occupying what appears to have been a genuine acropolis, suggests that this must have been the site of one of the oldest settlements at Dumat al-Jandal, dating at least back to Nabatean times.", landmarkImage:"MaridCastle",landmarkLocation: "Al-Jawf",landmarkDistance:"946 KM"),
         
         
         Landmark(landmarkTitle: "Madain Saleh", landmarkBrief: "It is located in the north of Madinah Province, in the area of Al-Ula, where many other fascinating archeological sites attest to the thousands of years of human occupation of the area thanks to the underground water available for millennia.", landmarkImage:"MadainSaleh",landmarkLocation: "Al-Ula",landmarkDistance:"1045 KM"),
+        
+        Landmark(landmarkTitle: "Jebel Al-Lawz", landmarkBrief: "The Jebel Al-Lawz, whose name means the almond mountain in Arabic, is one of the most mythical mountains of the Kingdom of Saudi Arabia. Most people think that Saudi Arabia is just a vast extent of sand dunes but with its summit at 2,549 meters above sea level Jebel Al-Lawz is the highest mountain of Tabuk Province", landmarkImage:"JabalAllawz",landmarkLocation: "Tabuk",landmarkDistance:"1572 KM"),
+        
+    
+   
+        Landmark(landmarkTitle: "Mugha’ir Shu’ayb", landmarkBrief: "Madyan is an archeological site located in the northwestern Arabian Peninsula, in the region of Tabuk, Saudi Arabia. The place consists of houses that have been cut from huge rocks and can be dated back to 2000 BC. It is one of the few sites that are famous for the Prophets that were sent there. This particular place signifies the name of Prophet Shoaib Peace Be Upon Him. ", landmarkImage:"Mugha’ir Shu’ayb",landmarkLocation: "Tabuk",landmarkDistance:"1581 KM"),
     
         
     ]
@@ -191,7 +193,7 @@ struct  Landmark : Identifiable{
 struct CardView: View {
     var landmark : Landmark
     var body: some View {
-        
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
         HStack(alignment:.top){
             Image(landmark.landmarkImage)
                 .resizable()
@@ -200,13 +202,14 @@ struct CardView: View {
                 .cornerRadius(10)
             
             VStack(alignment: .leading,spacing:5,content:  {
-                HStack{
+                HStack(spacing:5){
                     
                     Text(landmark.landmarkTitle)
                         .fontWeight(.heavy)
                         .foregroundColor(.color5)
                       
                     Spacer()
+                    
                     
                     VStack{
                         HStack(spacing:20){
@@ -221,12 +224,12 @@ struct CardView: View {
                             .foregroundColor(.color5)
                     }
             
-                       Image("pin")
+                       Image("Bitmap")
                         .resizable()
-                        .frame(width:20,height:20)
+                        .frame(width:16.84,height:20)
                         .foregroundColor(.color5)
-                      
-                        
+                
+                        .padding(.trailing)
                 
                 }
                 
@@ -246,7 +249,7 @@ struct CardView: View {
                            RoundedRectangle(cornerRadius:8)
                            .stroke(Color.color2,lineWidth:1)
                                                          )
-        
+        })
        
     }
 }
