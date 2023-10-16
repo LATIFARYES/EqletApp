473d238e4b74879a8f601c1ef243394644a349f4
WajdAlhadlaq <86900714+WajdAlhadlaq@users.noreply.github.com>
2023-10-16 16:09:16 +0300

    apply MyJourneys page

--- EqletApp/MyJourneys.swift
+++ EqletApp/MyJourneys.swift
@@ -19,25 +19,25 @@ struct MyJourneys: View {
                 
                 VStack(spacing: 30){
   
-                    CardView(title: "Al Masmak",
-                             content: "it is a strong thick building used as a depot for weapons and ammunitions turned into a historic milestone then a museum.",
-                             city: "Riyadh",
-                             location: 26,
-                             imageName: "MASMAK 3")
-                    
-                    
-                    CardView(title: "Al Diriyah.",
-                             content: "provides you the experience of wandering among a harmonious network of ancient areas and their monuments.",
-                             city: "Riyadh",
-                             
-                             location: 60,
-                             imageName: "DIRIYAH3")
-                    
-                    CardView(title: "Al Murabba",
-                             content: "you will notice the unique design of the foyer at the center of the palace, surrounded by all the rooms.",
-                             city: "Riyadh",
-                             location: 60,
-                             imageName: "MURABBA3")
+//                    CardView(title: "Al Masmak",
+//                             content: "it is a strong thick building used as a depot for weapons and ammunitions turned into a historic milestone then a museum.",
+//                             city: "Riyadh",
+//                             location: 26,
+//                             imageName: "MASMAK 3")
+//                    
+//                    
+//                    CardView(title: "Al Diriyah.",
+//                             content: "provides you the experience of wandering among a harmonious network of ancient areas and their monuments.",
+//                             city: "Riyadh",
+//                             
+//                             location: 60,
+//                             imageName: "DIRIYAH3")
+//                    
+//                    CardView(title: "Al Murabba",
+//                             content: "you will notice the unique design of the foyer at the center of the palace, surrounded by all the rooms.",
+//                             city: "Riyadh",
+//                             location: 60,
+//                             imageName: "MURABBA3")
 
 
 
@@ -67,94 +67,94 @@ struct MyJourneys: View {
 #Preview {
     MyJourneys()
 }
-
-struct CardView: View {
-    
-    var title: String
-    var content: String
-    
-    var city: String
-    var location: Int
-    
-    var imageName: String
-    
-    var body: some View {
-        HStack(alignment: .top) {
-            
-            Image(imageName)
-                .resizable()
-                .scaledToFill()
-                .frame(width: 130, height: 140)
-                .cornerRadius(16)
-            
-            
-            
-            VStack(alignment: .leading, spacing: 15) {
-                
-                HStack {
-                    
-                    
-                    
-                    Text(title)
-                        .font(.system(size: 16))
-                        .bold()
-                        .foregroundColor(.colortitle)
-                    
-                   
-                    Spacer()
-                    
-                    
-                    VStack {
-                        HStack(spacing: 2) {
-                            
-                            Image(systemName: "mappin.and.ellipse")
-                                .foregroundColor(.colortitle)
-                            
-                            VStack {
-                                Text(city)
-                                    .font(.system(size: 12))
-                                    .bold()
-                                    .foregroundColor(.colortext)
-                             
-                                Text("\(location) km")
-                                    .font(.system(size: 8))
-                                    .bold()
-                                    .foregroundColor(.colortitle)
-
-                            }
-                            
-                        }
-                        
-                        
-                    }
-                    
-         
 //
+//struct CardView: View {
+//    
+//    var title: String
+//    var content: String
+//    
+//    var city: String
+//    var location: Int
+//    
+//    var imageName: String
+//    
+//    var body: some View {
+//        HStack(alignment: .top) {
+//            
+//            Image(imageName)
+//                .resizable()
+//                .scaledToFill()
+//                .frame(width: 130, height: 140)
+//                .cornerRadius(16)
+//            
+//            
+//            
+//            VStack(alignment: .leading, spacing: 15) {
+//                
+//                HStack {
+//                    
+//                    
+//                    
 //                    Text(title)
 //                        .font(.system(size: 16))
 //                        .bold()
 //                        .foregroundColor(.colortitle)
-                    
-                    
-                }
-                .padding(.horizontal, 10)
-                .padding(.vertical, 5)
-                
-                Text(content)
-                    .font(.system(size: 14))
-                    .multilineTextAlignment(.leading)
-                    .padding(.horizontal, 10)
-                    .foregroundColor(.colortext)
-                
-            }
-
-            
-            
-        }
-        .frame(width: 350, height: 140)
-        .background(.white)
-        .cornerRadius(16)
-        .shadow(color: .gray, radius: 0.5, x: 0, y: 0)
-
-    }
-}
+//                    
+//                   
+//                    Spacer()
+//                    
+//                    
+//                    VStack {
+//                        HStack(spacing: 2) {
+//                            
+//                            Image(systemName: "mappin.and.ellipse")
+//                                .foregroundColor(.colortitle)
+//                            
+//                            VStack {
+//                                Text(city)
+//                                    .font(.system(size: 12))
+//                                    .bold()
+//                                    .foregroundColor(.colortext)
+//                             
+//                                Text("\(location) km")
+//                                    .font(.system(size: 8))
+//                                    .bold()
+//                                    .foregroundColor(.colortitle)
+//
+//                            }
+//                            
+//                        }
+//                        
+//                        
+//                    }
+//                    
+//         
+////
+////                    Text(title)
+////                        .font(.system(size: 16))
+////                        .bold()
+////                        .foregroundColor(.colortitle)
+//                    
+//                    
+//                }
+//                .padding(.horizontal, 10)
+//                .padding(.vertical, 5)
+//                
+//                Text(content)
+//                    .font(.system(size: 14))
+//                    .multilineTextAlignment(.leading)
+//                    .padding(.horizontal, 10)
+//                    .foregroundColor(.colortext)
+//                
+//            }
+//
+//            
+//            
+//        }
+//        .frame(width: 350, height: 140)
+//        .background(.white)
+//        .cornerRadius(16)
+//        .shadow(color: .gray, radius: 0.5, x: 0, y: 0)
+//
+//    }
+//}
